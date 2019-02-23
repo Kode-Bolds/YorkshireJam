e0541577e7508dc0a66b8852426a8e0f8dc7f72e
Anthony Beacock <tonybeacock@hotmail.co.uk>
2019-02-23 13:00:21 +0000

    Added SceneManager, Transitions and Audio Functionality

---
+++ Assets/Scripts/SceneManagerScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerScript : MonoBehaviour
+{
+    /// <summary>
+    /// Loads a specified scene
+    /// </summary>
+    /// <param name="pSceneName">Scene to load. (specified in inspector)</param>
+    public void LoadScene(string pSceneName)
+    {
+        SceneManager.LoadScene(pSceneName);
+    }
+
+    /// <summary>
+    ///  Unloaded a specified scene
+    /// </summary>
+    /// <param name="pSceneName">Scene to unload. (specified in inspector)</param>
+    public void UnloadScene(string pSceneName)
+    {
+        SceneManager.UnloadSceneAsync(pSceneName);
+    }
+
+    /// <summary>
+    /// Exits the game.
+    /// </summary>
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
