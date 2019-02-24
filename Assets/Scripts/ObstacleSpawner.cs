7493d9ccdfc5e6d1da5e04dcb243d6984da68f5c
Anthony Beacock <tonybeacock@hotmail.co.uk>
2019-02-24 13:51:17 +0000

    Random fences throughout the game

--- Assets/Scripts/ObstacleSpawner.cs
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -16,7 +16,7 @@ public class ObstacleSpawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -30,17 +30,17 @@ public class ObstacleSpawner : MonoBehaviour
 
         if(randomSpawnChance1 < spawnChance1 && Time.timeScale == 1)
         {
-            Instantiate(obstacle1, new Vector3(spawnPos, 1, player.transform.position.z + 40), new Quaternion());
+            Instantiate(obstacle1, new Vector3(spawnPos, 1, player.transform.position.z + 175), new Quaternion());
         }
 
         if (randomSpawnChance2 < spawnChance2 && Time.timeScale == 1)
         {
-            Instantiate(obstacle2, new Vector3(spawnPos, 1, player.transform.position.z + 40), new Quaternion());
+            Instantiate(obstacle2, new Vector3(spawnPos, 1, player.transform.position.z + 175), new Quaternion());
         }
 
         if (randomSpawnChance3 < spawnChance3 && Time.timeScale == 1)
         {
-            Instantiate(obstacle3, new Vector3(spawnPos, 1, player.transform.position.z + 40), new Quaternion());
+            Instantiate(obstacle3, new Vector3(spawnPos, 1, player.transform.position.z + 175), new Quaternion());
         }
     }
 }
